69d836bbdd763c44f14eab6639957955cfc4bb0b
2422june <ck66661254@gmail.com>
2022-06-09 15:49:49 +0900

    3

--- DropAndBoom/Assets/Scripts/BlockController.cs
+++ DropAndBoom/Assets/Scripts/BlockController.cs
@@ -7,7 +7,9 @@ public class BlockController : MonoBehaviour
     [SerializeField]
     float fallSpeed;
 
-    bool isBust;
+    bool isBust, isFalling, isLanding;
+
+    int line;
 
     Vector3 Vec;
 
@@ -15,50 +17,88 @@ public class BlockController : MonoBehaviour
 
     void Start()
     {
+        isFalling = false;
         isBust = false;
+        isLanding = false;
         fallSpeed = 10f;
+        line = 0;
 
         myRigid = GetComponent<Rigidbody>();
-
-        Vec = Vector3.down;
-        Vec *= fallSpeed;
-
-        myRigid.velocity = Vec;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !isBust)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Vec = Vector3.down;
-            Vec.y = fallSpeed * -5;
-            myRigid.velocity = Vec;
+            if(!isFalling)
+            {
+                myRigid.useGravity = true;
 
-            isBust = true;
+                Vec = Vector3.down;
+                Vec *= fallSpeed;
+                myRigid.velocity = Vec;
+
+                isFalling = true;
+            }
+            else if(!isBust)
+            {
+                Vec = Vector3.down;
+                Vec.y = fallSpeed * -5;
+                myRigid.velocity = Vec;
+
+                isBust = true;
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if(!isFalling)
         {
-            Vec = Vector3.zero;
-            Vec.x = 1.2f;
-            transform.position += Vec;
+           if (Input.GetKeyDown(KeyCode.RightArrow) && line != 4)
+            {
+                line++;
+                Vec = Vector3.zero;
+                Vec.x = 1.3f;
+                transform.position += Vec;
+            }
+            
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && line != -4)
+            {
+                line--;
+                Vec = Vector3.zero;
+                Vec.x = -1.3f;
+                transform.position += Vec;
+            }
         }
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        else
         {
-            Vec = Vector3.zero;
-            Vec.x = -1.2f;
-            transform.position += Vec;
+            if(!isLanding)
+            {
+                if (transform.position.y <= -8)
+                {
+                    Vec = transform.position;
+                    Vec.y = -8f;
+                    transform.position = Vec;
+
+                    CreateBlock();
+                }
+                else if(myRigid.velocity.y > -1)
+                {
+                    CreateBlock();
+                }
+            }
+
         }
+    }
 
-        if (transform.position.y <= -3.79f)
-        {
-            Vec = transform.position;
-            Vec.y = -3.79f;
 
-            myRigid.velocity = Vector3.zero;
+    void CreateBlock()
+    {
+        myRigid.velocity = Vector3.zero;
 
-            myRigid.useGravity = false;
-        }
+        Vec = Vector3.zero;
+        Vec.y = 8.7f;
+
+        Instantiate(Resources.Load<GameObject>("Prefabs/Block"), Vec, Quaternion.identity, null);
+
+        isLanding = true;
     }
 }
